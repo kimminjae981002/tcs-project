3a886bc0af948041d8c0ebd42d627b6832e62edc
kimminjae981002 <145568228+kimminjae981002@users.noreply.github.com>
2025-01-07 22:32:15 +0900

    [main]: 충돌 해결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TodoModule],
-=======
-
-@Module({
   imports: [],
->>>>>>> 2486ee6932599d15b3221525b577d17c7a8c6d52
   controllers: [AppController],
   providers: [AppService],
 })
